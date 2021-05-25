d2584ba7360a50aeade43350ad866e4362433f31
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-05-25 11:45:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
    
 }
+ function formatDay(timestamp){
+     let date = new Date(timestamp *1000);
+     let day = date.getDay();
+     let days =["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+     return days[day];
+ }
  function displayForecast(response){
 
     let forecastElement = document.querySelector("#forecast");
@@ -26,7 +33,7 @@ function formatDate(timestamp){
         forecastHTML = forecastHTML +
         `
             <div class="col-2">
-              <div class="weather-forecast-date">${forecastDay.dt}</div> 
+              <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div> 
                 <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
                 width="42"/>
                <div class="weather-forecast-temperature">
@@ -43,7 +50,7 @@ function formatDate(timestamp){
 
  function getForecast(coordinates){
      let apiKey = "e1011e97bf969d1b569c2b62944075b5 ";
-     let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+     let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=e1011e97bf969d1b569c2b62944075b&units=metric`;
 
      axios.get(apiUrl).then(displayForecast);
  }
@@ -111,3 +118,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search();
+displayForecast();
