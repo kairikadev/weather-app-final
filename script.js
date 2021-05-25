9d8b3ff8ff02ba3885a42fb7eaf98b0a8bc3b09a
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-05-25 11:18:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -22,16 +22,16 @@ function formatDate(timestamp){
     let forecast = response.data.daily;
     let forecastHTML = `<div class="row">`;
    
-    days.forEach(function(forecastDay){
+    forecast.forEach(function(forecastDay){
         forecastHTML = forecastHTML +
         `
             <div class="col-2">
               <div class="weather-forecast-date">${forecastDay.dt}</div> 
-                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}.png" 
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
                 width="42"/>
                <div class="weather-forecast-temperature">
                    <span class="weather-forecast-temperature-max">${forecastDay.temp.max}</span>
-                   <span class="weather-forecast-temperature-min">${forecast.temp.min}</span>
+                   <span class="weather-forecast-temperature-min">${forecastDay.temp.min}</span>
                </div> 
             </div> 
         
