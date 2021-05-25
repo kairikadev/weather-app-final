f3a98f014f82a25c24e146425d7028769aa29555
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-05-25 12:21:25 +0200

    Updating forecast from API function

--- script.js
+++ script.js
@@ -23,13 +23,14 @@ function formatDate(timestamp){
      return days[day];
  }
  function displayForecast(response){
-
+     
     let forecastElement = document.querySelector("#forecast");
     
     let forecast = response.data.daily;
     let forecastHTML = `<div class="row">`;
    
-    forecast.forEach(function(forecastDay){
+    forecast.forEach(function(forecastDay, index){
+        if(index < 5){
         forecastHTML = forecastHTML +
         `
             <div class="col-2">
@@ -37,12 +38,12 @@ function formatDate(timestamp){
                 <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
                 width="42"/>
                <div class="weather-forecast-temperature">
-                   <span class="weather-forecast-temperature-max">${forecastDay.temp.max}</span>
-                   <span class="weather-forecast-temperature-min">${forecastDay.temp.min}</span>
+                   <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}</span>
+                   <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}</span>
                </div> 
             </div> 
         
-        `;
+        `; }
     });
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
@@ -50,7 +51,7 @@ function formatDate(timestamp){
 
  function getForecast(coordinates){
      let apiKey = "e1011e97bf969d1b569c2b62944075b5 ";
-     let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=e1011e97bf969d1b569c2b62944075b&units=metric`;
+     let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=e1011e97bf969d1b569c2b62944075b5&units=metric`;
 
      axios.get(apiUrl).then(displayForecast);
  }
